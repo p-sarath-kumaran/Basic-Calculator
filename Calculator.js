27ff9c3a16bcce2acb50e82f099e469b8261f50d
P Sarath Kumaran <161850376+p-sarath-kumaran@users.noreply.github.com>
2024-03-02 14:20:49 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,27 @@
+let expression='';
+function addnumber(num) {
+    expression += num;
+    document.getElementById('display').value = expression;
+}
+function addoperator(operator) {
+    expression += operator;
+    document.getElementById('display').value = expression;
+}
+function rem() {
+    expression = '';
+    document.getElementById('display').value = expression;
+}
+function del() {
+    expression = expression.slice(0,-1);
+    document.getElementById('display').value = expression;
+}
+function calculate() {
+    try{
+        const result = eval(expression);
+        document.getElementById('display').value = result;
+        expression = result.toString();
+    } catch(error) {
+        document.getElementById('display').value = 'Error';
+        expression = '';
+    }
+}
